8db3ba5e78c559da38e5414b8ecf47ab894ca7cd
Damsbot64 <damien64durand@gmail.com>
2022-12-09 10:40:08 +0100

    change post for put

--- src/api/Collection/routes.ts
+++ src/api/Collection/routes.ts
@@ -7,7 +7,7 @@ router.get("/", controllers.getAll);
 router.get("/", controllers.getOne);
 
 router.post("/", controllers.create);
-router.post("/:id", controllers.update);
+router.put("/:id", controllers.update);
 router.delete("/:id", controllers.delete);
 
 export default router;
